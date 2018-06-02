ac343c651914eb9c7962a28db95d3d801f78f246
christopher crew baker <chriscrewbaker@gmail.com>
2018-06-01 23:49:02 -0400

    add basic example

---
+++ examples/basic-example.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+# i'm creating a plot here
+plt.plot([1,2,3,4]);
+plt.ylabel('some numbers');
+plt.show()
+
+# here is a comment
+# here is another comment
+aa = "aa"
+k = "ok then" + \
+    "hi there"
+# another comment
+y = "hi "; z = "ok then" + \
+    "ok then"
+k
+
+# here's how you print:
+print(k)
+
+# another comment
+something = "ok then"
+something
+
+## cool function
+def some_fun():
+    print("ok")
+    print("yas")
+
+
+some_fun()
+
+10 * (1/0)
