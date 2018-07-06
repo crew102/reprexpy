af975fe94d1560cf82ea0c71cc0cd1c7dfbf7903
Chris Baker <chriscrewbaker@gmail.com>
2018-07-06 15:30:37 -0400

    move dir containing examples used for testing

--- tests/test_reprexpy.py
+++ tests/test_reprexpy.py
@@ -9,14 +9,14 @@ def read_ex_fi(file):
 
 
 def test_statement_parser():
-    code_str = read_ex_fi("../examples/basic-example.py")
+    code_str = read_ex_fi("tests/test-examples/basic-example.py")
     s_chunks = _get_statement_chunks(code_str)
     assert len(s_chunks) == 21
 
 
 # todo: refactor this so _get_statement_chunks isn't tested twice
 def test_nb_exe():
-    code_str = read_ex_fi("../examples/basic-example.py")
+    code_str = read_ex_fi("tests/test-examples/basic-example.py")
     s_chunks = _get_statement_chunks(code_str)
     out = _run_nb(s_chunks, "python3")
     assert isinstance(out, type(nbformat.v4.new_notebook()))
