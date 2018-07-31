9fd6e9aaf2459536d28723a522cce93ace8fbf59
christopher crew baker <chriscrewbaker@gmail.com>
2018-07-30 21:31:35 -0400

    use link to github page for long description

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
 from setuptools import setup
-from codecs import open
-from os import path
-
-# Get the long description from the README file
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='reprexpy',
     version='0.1.0dev',
     description='Render reproducible examples of code (port of R package `reprex`)',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description='See https://github.com/crew102/reprexpy for details',
     author='Christopher Baker',
     author_email='chriscrewbaker@gmail.com',
     url='https://github.com/crew102/reprexpy/issues',
