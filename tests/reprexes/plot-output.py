77dcb0856bcba17b44887ad784a9e87b689c4ebf
Chris Baker <chriscrewbaker@gmail.com>
2018-08-16 16:36:47 -0400

    make small changes to test example file

--- tests/reprexes/plot-output.py
+++ tests/reprexes/plot-output.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
 
+data = [1, 2, 3, 4]
+
 # i'm creating a plot here
-plt.plot([1,2,3,4]);
+plt.plot(data);
 plt.ylabel('some numbers');
 plt.show()
 plt.close()
 
 # another plot
-plt.plot([1,2,3,4]);
-plt.ylabel('some more numbers');
+plt.plot(data);
+plt.xlabel('more numbers');
 plt.show()
 plt.close()
 
 # final plot
-plt.plot([1,2,3,4]);
+plt.plot(data);
 plt.show()
