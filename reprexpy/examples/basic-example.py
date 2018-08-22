883fb27bf4df4daa583840674a15af9c5a3c34fd
Chris Baker <chriscrewbaker@gmail.com>
2018-08-22 12:22:22 -0400

    use a more realistic basic example

--- reprexpy/examples/basic-example.py
+++ reprexpy/examples/basic-example.py
@@ -1,3 +1,7 @@
-x = "hi there"
-y = " old friend"
-print(x + y)
+# i know you can flatten a list in python using list comprehension:
+l = [[1, 2, 3], [4, 5, 6], [7], [8, 9]]
+[item for sublist in l for item in sublist]
+
+# but i'd like to know if there's another way. i tried this but i got an error:
+import functools
+functools.reduce(lambda x, y: x.extend(y), l)
