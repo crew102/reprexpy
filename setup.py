9a385a9753013319a6ae918d964aa42a6b2221f0
Chris Baker <chriscrewbaker@gmail.com>
2018-08-22 17:09:36 -0400

    don't use encoding param

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info[0] == 2:
     install_requires.append('ipython<6')
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(
