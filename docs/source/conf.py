55350ad03d28167cb383f7fd8361f046eb5fe9c9
Chris Baker <chriscrewbaker@gmail.com>
2018-08-27 15:25:10 -0400

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ author = u'Christopher Baker'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.0.0.9000'
+release = u'0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
