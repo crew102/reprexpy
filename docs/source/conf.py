f6bd42bb834fe8221ae20bb0ee0c81c3c2b20ce0
christopher crew baker <chriscrewbaker@gmail.com>
2019-01-01 21:14:17 -0500

    drop duplicate line in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,6 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
 source_suffix = ['.rst', '.md']
 
 # source_parsers = {
