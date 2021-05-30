8647157438e2b66b0f5f6f3b6cfb2e2d7b45172e
christopher crew baker <chriscrewbaker@gmail.com>
2021-05-30 18:37:27 -0400

    Use m2r2 instead of m2r for sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'm2r'
+    'm2r2'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
