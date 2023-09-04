e074afad5a3e1417be11855bb1ccb7a7d3419fb8
chris <chriscrewbaker@gmail.com>
2023-09-04 13:17:30 -0400

    Update license date and build number referenced in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,13 +19,13 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = u'reprexpy'
-copyright = u'2019, Christopher Baker'
+copyright = u'2023, Christopher Baker'
 author = u'Christopher Baker'
 
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.3.1'
+release = u'0.3.3'
 
 # -- General configuration ---------------------------------------------------
 
@@ -63,7 +63,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+# language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
