d341be5200af0aeb6654b500cf4d01c0b7be09ef
Chris Baker <chriscrewbaker@gmail.com>
2026-05-23 00:49:25 -0400

    Bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 
 setup(
     name='reprexpy',
-    version='0.3.3',
+    version='0.3.4',
     description='Render reproducible examples of Python code (port of R '
                 'package `reprex`)',
     long_description=long_description,
